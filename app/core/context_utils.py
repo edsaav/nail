9f6f5614b3acd07779f73550774493175e01e190
Edward Saavedra <edsaav@gmail.com>
2023-04-13 20:38:34 -0700

    feat: add ability to build context from directory

--- app/core/context_utils.py
+++ app/core/context_utils.py
@@ -1,4 +1,5 @@
 from app.core.file_utils import read_file
+import os
 
 CONTEXT_PREFIX = "Existing files for context:\n\n"
 CONTEXT_SUFFIX = "Given the above context, draft a new file using the following request:\n\n"
@@ -28,3 +29,35 @@ def format_file_label(file_path):
 
 def format_file_block(context_file_path, context_content):
     return f"{format_file_label(context_file_path)}\n```\n{context_content}\n```\n\n"
+
+
+def list_all_files(path=None):
+    """
+    Recursively list all file paths within the given directory, including files
+    nested within sub-directories.
+
+    :param path: Optional path to the directory. If not provided, uses the current path.
+    :return: List of strings containing file paths within the directory.
+    """
+    if path is None:
+        path = os.getcwd()
+
+    file_paths = []
+
+    for root, _, files in os.walk(path):
+        for file in files:
+            file_paths.append(os.path.join(root, file))
+
+    return file_paths
+
+
+def build_context_prefix_from_directory(path=None):
+    """
+    Build the context prefix string based on all the files in the specified (or current) directory.
+
+    :param path: Optional path to the directory. If not provided, uses the current path.
+    :return: context_prefix: String containing the context prefix
+    """
+    context_file_paths = list_all_files(path)
+    context_prefix = build_context_prefix(context_file_paths)
+    return context_prefix
