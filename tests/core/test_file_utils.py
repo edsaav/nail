1bc16538e97538ad5918774a7ee2bad25260cc05
Edward Saavedra <edsaav@gmail.com>
2023-04-18 17:47:36 -0700

    feat: add ability to generate diff

--- tests/core/test_file_utils.py
+++ tests/core/test_file_utils.py
@@ -1,8 +1,8 @@
 import os
 import tempfile
 import pytest
-from unittest.mock import patch
-from app.core.file_utils import read_file, write_file, open_editor
+from unittest.mock import patch, call
+from app.core.file_utils import read_file, write_file, open_editor, diff_and_confirm, print_colored_diff_line
 
 
 def test_read_file():
@@ -57,5 +57,25 @@ def test_open_editor_with_custom_editor(mock_call):
         os.environ.update(old_environ)
 
 
-if __name__ == "__main__":
-    pytest.main()
+@patch("app.core.file_utils.input")
+@patch("app.core.file_utils.read_file")
+@patch("app.core.file_utils.print_colored_diff_line")
+def test_diff_and_confirm(mock_print_colored_diff_line, mock_read_file, mock_input):
+    # Mock the read_file function to return a fixed content
+    mock_read_file.return_value = "Hello, World!"
+    # Mock the user input to return 'y' for confirmation
+    mock_input.return_value = 'y'
+
+    # Test diff_and_confirm function
+    file_path = "test.txt"
+    new_content = "Hello, New World!"
+    result = diff_and_confirm(file_path, new_content)
+    assert result == True
+
+    # Assert that print_colored_diff_line is called with the correct diff lines
+    expected_calls = [
+        call('-Hello, World!'),
+        call('+Hello, New World!'),
+    ]
+    mock_print_colored_diff_line.assert_has_calls(
+        expected_calls, any_order=True)
