423dccb62ecc4343f00a551d61d0340c0efb6a03
Edward Saavedra <edsaav@gmail.com>
2023-04-28 23:24:47 -0700

    refactor: extract loading animation into decorator

--- nail/core/chat_utils.py
+++ nail/core/chat_utils.py
@@ -1,8 +1,8 @@
 import re
 import openai
-from threading import Event, Thread
+
 from nail.core.config_utils import get_api_key
-from nail.core.animation_utils import display_loader
+from nail.core.loading_decorator import loadable
 
 DEFAULT_SYSTEM_MESSAGE = "You are a helpful assistant."
 CODE_SYSTEM_MESSAGE = """You are a code generating assistant.
@@ -36,22 +36,15 @@ def predict(
         user_message(prompt)
     ]
 
-    # Start a thread to display the loading animation
-    # while the predict function is working
-    stop_loading = Event()
-    loader_thread = Thread(target=loading, args=(stop_loading,))
-    loader_thread.start()
+    create_completion = loadable(
+        openai.ChatCompletion.create, 'Generating code... ')
 
-    response = openai.ChatCompletion.create(
+    response = create_completion(
         model=model,
         messages=messages,
         temperature=DEFAULT_TEMPERATURE,
         max_tokens=DEFAULT_MAX_TOKENS,
     )
-
-    # Stop the loading animation thread once the response is received
-    stop_loading.set()
-    loader_thread.join()
 
     return response.choices[0].message["content"]
 
@@ -79,8 +72,3 @@ def user_message(message):
 
 def system_message(message):
     return {"role": "system", "content": message}
-
-
-def loading(stop_loading):
-    while not stop_loading.is_set():
-        display_loader(stop_loading, "Generating code...  ")
--- nail/core/loading_decorator.py
+++ nail/core/loading_decorator.py
@@ -2,7 +2,12 @@ import itertools
 import sys
 import time
 
-frames = [
+from threading import Event, Thread
+
+ANIMATION_FRAME_SECONDS = 0.04
+DEFAULT_LOADING_MESSAGE = 'Loading...'
+
+FRAMES = [
     "|==========>                    |",
     " |==========>                   |",
     "  |==========>                  |",
@@ -54,14 +59,42 @@ frames = [
 ]
 
 
-def display_loader(stop_loading, prefix="Loading...  "):
-    """
-    Displays a loading animation of a nail.
-    """
-    spinner = itertools.cycle(frames)
+def loadable(func, loading_message):
+    '''
+    This decorator is used to display a loading animation
+    while a function is running.
+
+    :param func: The function to be decorated
+    :param loading_message: The message to be displayed before the animation
+    '''
+    def wrapper(*args, **kwargs):
+        # Start a thread to display the loading animation
+        stop_loading = Event()
+        loader_thread = Thread(target=_loading, args=(
+            stop_loading, loading_message,))
+        loader_thread.start()
+
+        result = func(*args, **kwargs)
+
+        # Stop the loading animation thread once the response is received
+        stop_loading.set()
+        loader_thread.join()
+
+        return result
+
+    return wrapper
+
+
+def _loading(stop_loading, loading_message):
+    while not stop_loading.is_set():
+        _display_loader(stop_loading, loading_message)
+
+
+def _display_loader(stop_loading, prefix=DEFAULT_LOADING_MESSAGE):
+    spinner = itertools.cycle(FRAMES)
     while not stop_loading.is_set():
         sys.stdout.write(f"{prefix}{next(spinner)}")
         sys.stdout.flush()
-        time.sleep(0.04)
+        time.sleep(ANIMATION_FRAME_SECONDS)
         sys.stdout.write('\r')
     sys.stdout.write('\n')
