86c2548beb7804dabe2b9cb2766dfe6f0ab02361
Edward Saavedra <edsaav@gmail.com>
2023-04-30 12:05:32 -0700

    test: update tests to use new FileEditor

--- tests/core/test_context_utils.py
+++ tests/core/test_context_utils.py
@@ -27,10 +27,13 @@ def test_build_context_prefix_empty_list():
     assert build_context_prefix([]) == ""
 
 
-@patch("nail.core.context_utils.read_file", return_value=test_file_content)
 @patch("nail.core.context_utils.format_file_block", return_value=test_file_content)
-def test_build_context_prefix_with_files(mock_format_file_block, mock_read_file):
-    context_file_paths = [test_file_path]
+def test_build_context_prefix_with_files(_, tmp_path):
+    # Create a test file
+    file_path = tmp_path / test_file_path
+    file_path.write_text(test_file_content)
+
+    context_file_paths = [file_path]
     expected_output = "Existing files for context:\n\nThis is a test file." \
         + "Given the above context, draft a new file using the " \
         + "following request:\n\n"
--- tests/tools/test_modify.py
+++ tests/tools/test_modify.py
@@ -1,70 +1,22 @@
-from unittest.mock import patch, ANY
+from unittest.mock import patch
 from nail.tools.modify.modify_file import modify_file
 
 
-@patch("nail.tools.modify.modify_file.apply_changes")
-@patch("nail.tools.modify.modify_file.read_file")
 @patch("nail.tools.modify.modify_file.predict_code")
 @patch("nail.tools.modify.modify_file.build_context_prefix")
-def test_modify_file_with_optional_context_files(mock_build_context_prefix, mock_predict_code, mock_read_file, mock_apply_changes):
-    mock_file_content = "This is the original file content."
-    mock_request = "Please add a new line at the end."
-    mock_modified_content = "This is the modified file content."
-    mock_context_file_paths = ["context_file_1", "context_file_2"]
-    mock_additional_file_context = "Additional context from files."
+@patch('nail.tools.modify.modify_file.FileEditor')
+def test_modify_file(MockFileEditor, mock_build_context_prefix, mock_predict_code):
+    # Create a mock instance of FileEditor
+    mock_file_editor = MockFileEditor.return_value
 
-    mock_predict_code.return_value = mock_modified_content
-    mock_read_file.return_value = mock_file_content
-    mock_build_context_prefix.return_value = mock_additional_file_context
+    # Set the return values and side effects for the mock_file_editor instance
+    mock_file_editor.content.return_value = 'original content'
+    mock_predict_code.return_value = 'expected modified content'
+    mock_build_context_prefix.return_value = 'context prefix'
 
-    modify_file("dummy_file_path", mock_request,
-                context_file_paths=mock_context_file_paths)
+    # Call the modify_file function with the mocked instance
+    modify_file('path/to/file', 'request', context_file_paths=None, model=None)
 
-    # Assertions
-    mock_read_file.assert_called_once_with("dummy_file_path")
-    mock_build_context_prefix.assert_called_once_with(mock_context_file_paths)
-    mock_predict_code.assert_called_once()
-    mock_apply_changes.assert_called_once_with(
-        "dummy_file_path", mock_modified_content)
-
-
-@patch("nail.tools.modify.modify_file.apply_changes")
-@patch("nail.tools.modify.modify_file.read_file")
-@patch("nail.tools.modify.modify_file.predict_code")
-def test_modify_file_without_context_files(mock_predict_code, mock_read_file, mock_apply_changes):
-    mock_file_content = "This is the original file content."
-    mock_request = "Please add a new line at the end."
-    mock_modified_content = "This is the modified file content."
-
-    mock_predict_code.return_value = mock_modified_content
-    mock_read_file.return_value = mock_file_content
-
-    modify_file("dummy_file_path", mock_request)
-
-    # Assertions
-    mock_read_file.assert_called_once_with("dummy_file_path")
-    mock_predict_code.assert_called_once()
-    mock_apply_changes.assert_called_once_with(
-        "dummy_file_path", mock_modified_content)
-
-
-@patch("nail.tools.modify.modify_file.apply_changes")
-@patch("nail.tools.modify.modify_file.read_file")
-@patch("nail.tools.modify.modify_file.predict_code")
-def test_modify_file_with_specified_model(mock_predict_code, mock_read_file, mock_apply_changes):
-    mock_file_content = "This is the original file content."
-    mock_request = "Please add a new line at the end."
-    mock_modified_content = "This is the modified file content."
-    mock_model = "gpt-4"
-
-    mock_predict_code.return_value = mock_modified_content
-    mock_read_file.return_value = mock_file_content
-
-    modify_file("dummy_file_path", mock_request, model=mock_model)
-
-    # Assertions
-    mock_read_file.assert_called_once_with("dummy_file_path")
-    mock_predict_code.assert_called_once_with(
-        ANY, model=mock_model)
-    mock_apply_changes.assert_called_once_with(
-        "dummy_file_path", mock_modified_content)
+    # Assert the expected behavior
+    mock_file_editor.apply_changes.assert_called_once_with(
+        'expected modified content')
