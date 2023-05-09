60bae060415ea9fc864d3581307d1147962c750b
Edward Saavedra <edsaav@gmail.com>
2023-05-08 18:45:23 -0700

    build: add rich req

--- setup.py
+++ setup.py
@@ -5,25 +5,26 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='nail',
-    version='0.1.1',
-    description='A CLI tool for speeding up development using LLMs',
+    name="nail",
+    version="0.1.1",
+    description="A CLI tool for speeding up development using LLMs",
     packages=find_packages(),
     install_requires=[
-        'click',
-        'openai',
-        'termcolor',
-        'PyYAML',
+        "click",
+        "openai",
+        "termcolor",
+        "PyYAML",
+        "rich",
     ],
-    entry_points='''
+    entry_points="""
         [console_scripts]
         nail=nail.main:main
-    ''',
+    """,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Edward Saavedra',
-    author_email='edsaav@gmail.com',
-    url='https://github.com/edsaav/nail',
+    long_description_content_type="text/markdown",
+    author="Edward Saavedra",
+    author_email="edsaav@gmail.com",
+    url="https://github.com/edsaav/nail",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
